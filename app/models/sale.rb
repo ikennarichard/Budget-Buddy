88b35fac5d0ddbdbbfbaa5f868cdcee00de5885d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-10 11:33:59 -1100

    Update sale associations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,5 @@
 class Sale < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :groups
+  belongs_to :group
   validates :name, :amount, presence: true
 end
