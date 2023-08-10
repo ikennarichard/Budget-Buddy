5504de18874945d4da3ae7a02ae437f289fc362c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-10 11:33:25 -1100

    Update group model associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :sales 
+  has_many :sales, dependent: :destroy
   validates :name, :icon, presence: true
 end
