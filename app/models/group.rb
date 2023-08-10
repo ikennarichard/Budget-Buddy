fb2c52337b2ddfe127339a00fd774fb989be814e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-10 11:37:01 -1100

    Update model validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :sales, dependent: :destroy
-  validates :name, :icon, presence: true
+  validates :name, uniqueness: true, presence: true 
+  validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :sales, foreign_key: :author_id, dependent: destroy
-  has_many :groups, foreign_key: :author_id, dependent: destroy
+  has_many :sales, foreign_key: :author_id, dependent: :destroy
+  has_many :groups, foreign_key: :author_id, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Not a valid email address' }
 end
