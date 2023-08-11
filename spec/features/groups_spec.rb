72243943c4a1bb1327bc8bf891dcc0edaa9539f4
Ikenna Richard <oguejioforrichard@gmail.com>
2023-08-10 13:42:22 -1100

    Fix linters

--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -8,13 +8,11 @@ RSpec.feature 'Groups', type: :feature do
     group = create(:group, author: user)
 
     visit groups_path
-
     within '.list-unstyled' do
       expect(page).to have_content(group.name.capitalize)
       expect(page).to have_content(group.created_at.strftime('%d %B %Y'))
       expect(page).to have_content("$#{group.sales.sum(:amount)}")
     end
-
     within '.add-category' do
       expect(page).to have_link('add category'.upcase!, href: new_group_path)
     end
@@ -22,9 +20,7 @@ RSpec.feature 'Groups', type: :feature do
 
   it 'should display "No category added yet" when no categories are present' do
     sign_in user
-
     visit groups_path
-
     expect(page).to have_content('No category added yet, click add category')
   end
 
@@ -36,22 +32,17 @@ RSpec.feature 'Groups', type: :feature do
 
   it 'should add a new category and redirect to groups page' do
     sign_in user
-    group = create(:group, author: user)
+    create(:group, author: user)
 
     visit groups_path
 
     within '.add-category' do
       click_link 'add category'.upcase!
     end
-
-
     expect(page).to have_current_path(new_group_path)
-
     fill_in 'group_name', with: 'New category'
     fill_in 'group_icon', with: 'pic-1.jpg'
-
     click_button 'SAVE'
-
     expect(page).to have_content('Category was added successfully.')
     expect(page).to have_content('New category')
     expect(page).to have_css('.icon-image')
